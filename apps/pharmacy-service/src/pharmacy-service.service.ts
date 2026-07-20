aa3a27f0574d60db5f262ee9bbcd405ea7ef0c4b
IconicFish12 <superglidingogre0571@gmail.com>
2026-07-20 19:32:27 +0700

    project-chores: update dependencies and system service database initialization (just init not modified yet)

--- apps/pharmacy-service/src/pharmacy-service.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PharmacyServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
--- apps/ward-service/src/ward-service.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class WardServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
