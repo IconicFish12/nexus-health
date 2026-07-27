13a1bd07fab2edcecfa5f83edd8e412d43a9f50b
IconicFish12 <superglidingogre0571@gmail.com>
2026-07-28 00:00:26 +0700

    chores: add shared libraries, and installing main dependencies too each system service

--- libs/protobuf/src/protobuf.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProtobufService } from './protobuf.service';
-
-@Module({
-  providers: [ProtobufService],
-  exports: [ProtobufService],
-})
-export class ProtobufModule {}
