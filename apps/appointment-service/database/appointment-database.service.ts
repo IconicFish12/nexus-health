d0349f4d9ca3d80b2d1c6b1ae4d35b351c4476e5
IconicFish12 <superglidingogre0571@gmail.com>
2026-08-04 20:54:34 +0700

    chore-feat: setup database service configuration and setup database migrations

--- apps/appointment-service/database/appointment-database.service.ts
+++ apps/appointment-service/database/appointment-database.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma/client.ts';
 
 @Injectable()
-export class AppointmentDatabaseService {}
+export class AppointmentDatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private config: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: config.get<string>('DATABASE_URL'),
+    });
+    super({ adapter: adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
