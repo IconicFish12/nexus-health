b9aece3bf650e505003c81f8829329a3ff0cd206
IconicFish12 <superglidingogre0571@gmail.com>
2026-08-05 18:30:37 +0700

    chores-feat: update system service compiling setup and import pattern, and update Dockerfile

--- out/full/apps/appointment-service/src/appointment-service.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppointmentServiceController } from './appointment-service.controller.ts';
-import { AppointmentServiceService } from './appointment-service.service.ts';
-
-@Module({
-  controllers: [AppointmentServiceController],
-  providers: [AppointmentServiceService],
-})
-export class AppointmentServiceModule {}
--- out/full/apps/appointment-service/src/appointment-service.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppointmentServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
