24d19ce219dd829a08bdf8589ad54e5dbd3b39a2
IconicFish12 <superglidingogre0571@gmail.com>
2026-08-06 17:04:18 +0700

    chores-feat: update Dockerfile and update file imports pattern

--- apps/pharmacy-service/src/main.ts
+++ apps/pharmacy-service/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { PharmacyServiceModule } from './pharmacy-service.module.ts';
+import { PharmacyServiceModule } from './pharmacy-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PharmacyServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.SERVICE_HTTP_PORT ?? 5007);
 }
 bootstrap();
